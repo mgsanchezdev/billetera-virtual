0e1d402c212020790f4798defaf23da6516b532d
Mario Sanchez <mgsanchezdev@gmail.com>
2021-02-07 17:03:06 -0300

    Funcionalidad de cargar salgo comunicacion entre el frontend y el backend

--- src/Pages/Dinero.js
+++ src/Pages/Dinero.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
 import { HeaderComponent } from '../Component/Layout/HeaderComponent';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import '../Pages/Login/login.css';
 
 export const Dinero = () => {
+  let extraida = '';
+
   const handlerCargaSaldo = async (e) => {
+    e.preventDefault();
+    const [documento, celular, saldo] = e.target;
+
     let xmls = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.soap.com/">
     <soapenv:Header/>
     <soapenv:Body>
        <ser:cargarSaldo>
           <!--Optional:-->
-          <documento>30437161</documento>
+          <documento>${documento.value}</documento>
           <!--Optional:-->
-          <celular>2314610149</celular>
-          <saldo>500</saldo>
+          <celular>${celular.value}</celular>
+          <saldo>${saldo.value}</saldo>
        </ser:cargarSaldo>
     </soapenv:Body>
  </soapenv:Envelope>`;
@@ -31,12 +37,14 @@ export const Dinero = () => {
         let dataString = JSON.stringify(res.data);
         let indice = dataString.indexOf('<return>');
         let indicefin = dataString.indexOf('</return>');
-        let extraida = dataString.substring(indice + 8, indicefin);
+        extraida = dataString.substring(indice + 8, indicefin);
+        Swal.fire('', `${extraida}`, 'success');
       })
       .catch((err) => {
         console.log(err);
       });
   };
+
   return (
     <>
       {' '}
@@ -45,33 +53,24 @@ export const Dinero = () => {
         <Form onSubmit={handlerCargaSaldo}>
           <Form.Group
             controlId="formBasicEmail"
-            className="login-margin-top input-width"
+            className=" dinero-margin-top input-width"
           >
             <Form.Label>Ingrese el documento </Form.Label>
             <Form.Control type="text" placeholder="52015620" />
           </Form.Group>
-          <Form.Group
-            controlId="formBasicEmail"
-            className="login-margin-top input-width"
-          >
-            <Form.Label>Ingrese el Email </Form.Label>
-            <Form.Control type="email" placeholder="1200" />
+          <Form.Group controlId="formBasicEmail" className=" input-width">
+            <Form.Label>Ingrese el numero de celular </Form.Label>
+            <Form.Control type="text" placeholder="1200" />
           </Form.Group>
-          <Form.Group
-            controlId="formBasicEmail"
-            className="login-margin-top input-width"
-          >
+          <Form.Group controlId="formBasicEmail" className=" input-width">
             <Form.Label>
               Cargue el saldo que desea a su billetera virtual{' '}
             </Form.Label>
             <Form.Control type="text" placeholder="1200" />
           </Form.Group>
-
-          <Link to="/home">
-            <Button className="bt-login" variant="success" type="submit">
-              Cargar saldo
-            </Button>
-          </Link>
+          <Button className="bt-login" variant="success" type="submit">
+            Cargar saldo
+          </Button>
         </Form>
       </Container>
     </>
